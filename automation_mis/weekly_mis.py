47b3d2b55e875cd0c1bdc3c14b2bb4e1df1e9c89
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-12-06 20:57:16 +0530

    feat/creating dataframe from google sheet

--- automation_mis/weekly_mis.py
+++ automation_mis/weekly_mis.py
@@ -6,5 +6,8 @@ os.chdir('C:\\Users\\panka\\github_repo\\Analytics\\automation_mis')
 gc = gspread.service_account(filename='credential.json')
 sh = gc.open_by_key('1c8GkIHjMkcmapbvEwDkBSQACzChlLROnfPXPlJJ8Txs')
 data = sh.worksheet('data_transfer').get_all_values()
-df = pd.DataFrame(data)
-print('hello world')
+
+df = pd.DataFrame(data, columns=data[0])
+df = df.drop([0])
+
+
