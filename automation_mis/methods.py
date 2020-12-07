a5224bf2583258556676a55f996e95b8594bac38
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-12-07 07:27:18 +0530

    feat/removed indentation error and EOF parse error

--- automation_mis/methods.py
+++ automation_mis/methods.py
@@ -4,14 +4,13 @@ from exchangelib import Configuration, DELEGATE, FileAttachment
 import os
 
 
-def create html_table(df, vendor):
+def create_html(df, vendor):
     df = df[df['Dependency'] == vendor]
     df = df.drop(columns=[
         'S No',
         'RO Code',
         'Mode of Transfer'
         ])
-
     html_table = df.to_html()
     html_content = """
     <html><body><h5>Pl find the list of issues due to which real time data transfer
@@ -30,32 +29,27 @@ def create html_table(df, vendor):
     return html_content
 
 
-def send_mail(html_content):
+def send_mail(html_content, cc_list, vendor):
     outlook_user = os.environ.get('OUTLOOK_USER')
     outlook_password = os.environ.get('OUTLOOK_PASS')
     outlook_server = os.environ.get('OUTLOOK_SERVER')
     outlook_email = os.environ.get('OUTLOOK_EMAIL')
-
     credentials = Credentials(
             username=outlook_user,
             password=outlook_password)
-
     config = Configuration(
             server=outlook_server,
             credentials=credentials)
-
     account = Account(
             primary_smtp_address=outlook_email,
             config=config,
             autodiscover=False,
             access_type=DELEGATE)
-
     msg = Message(
             account=account,
-            subject="Data Transfer - ORPAK depenency - Sambalpur DO",
+            subject=f'Data Transfer - {vendor} depenency - Sambalpur DO',
             body=HTMLBody(html_content),
             to_recipients=['barnwalp@indianoil.in'],
-#            cc_recipients=['kumardeepak1@indianoil.in', 'grana@indianoil.in'])
-
-
+            #cc_recipients=cc_list)
+            )
     msg.send_and_save()
