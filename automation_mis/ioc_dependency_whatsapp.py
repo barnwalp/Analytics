f21f53474256de72e72954265154071d3197e771
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-12-11 06:35:51 +0530

    feat/added whatsapp feature to weekly MIS script

--- automation_mis/ioc_dependency_whatsapp.py
+++ automation_mis/ioc_dependency_whatsapp.py
@@ -1,11 +1,6 @@
 import gspread
 import pandas as pd
 import os
-from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 
 
 #change currnet directory
